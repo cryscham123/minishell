573c202d07bd34b66f3fc1b7a077d6f4a5f440e9
Hyunghoon Kim <hyunghki@c8r7s11.42seoul.kr>
2023-06-11 15:49:42 +0900

    made parsing

--- hyunghki/srcs/main.c
+++ hyunghki/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hyunghki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 07:05:22 by hyunghki          #+#    #+#             */
-/*   Updated: 2023/06/11 15:38:31 by hyunghki         ###   ########.fr       */
+/*   Updated: 2023/06/11 15:46:12 by hyunghki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,12 @@ int	main(int argc, char **argv, char **env)
 	t_token_v	tv;
 
 	(void)argv;
+	(void)env;
 	if (argc != 1)
-		// error handle
+	{
+		printf("argument error\n");
+		return (0);
+	}
 	while (1)
 	{
 		tv.cur_token = NULL;
@@ -28,11 +32,15 @@ int	main(int argc, char **argv, char **env)
 		if (line != NULL && *line)
 		{
 			add_history(line);
-			if (ft_parse_data(&tv, line) != 0);
-				// error handle
-			ft_exe(&tv, env);
+			if (ft_parse_data(&tv, line) != 0)
+			{
+				printf("error occured...\n");
+				return (0);
+			}
+			//ft_exe(&tv, env);
 		}
-		ft_free_all(line, tv->cur_token);
+		free(line);
+		//ft_free_all(line, tv->cur_token);
 	}
 	return (0);
 }
