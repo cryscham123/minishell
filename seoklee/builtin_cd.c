38ad879708a654c0d442b33e504441e761156a33
lucku <lsw6731@naver.com>
2023-06-17 18:35:09 +0900

    builtin_cd

---
+++ seoklee/builtin_cd.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_cd.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seoklee <seoklee@student.42.kr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/17 14:09:47 by seoklee           #+#    #+#             */
+/*   Updated: 2023/06/17 18:33:03 by seoklee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+size_t	ft_strlen(const char *s)
+{
+	int		i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	i = ft_strlen(s1);
+	j = ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) * (i + j + 1));
+	if (str == NULL)
+		return (NULL);
+	i = -1;
+	while (s1[++i])
+		str[i] = s1[i];
+	j = -1;
+	while (s2[++j])
+		str[i + j] = s2[j];
+	str[i + j] = '\0';
+	free(s1);
+	return (str);
+}
+
+char	*get_env_value(t_lst *value)
+{
+	char	*str;
+
+	if (value == NULL)
+		return (NULL);
+	str = ft_substr(value->data, 1);
+	//if (str == NULL)
+	while (value->nxt != NULL)
+	{
+		str = ft_strjoin(str, value->nxt->data); //첫 인자 str은 strjoin 함수 안에서 free
+		//if (str == NULL)
+		value = value->nxt;
+	}
+	return (str);
+}
+
+int	change_dir(char *path, t_lst *ev)
+{
+	char	 cwd[4096];
+	t_lst	*value;
+
+	getcwd(cwd, 4096);
+	if (!chdir(path))
+	{
+		g_status = 0;
+		value = ft_hash_find(ev, "OLDPWD");
+		ft_lst_free(value, f_data_char, NULL);
+		value = mk_str_lst(cwd);
+		value = ft_hash_find(ev, "PWD");
+		ft_lst_free(value, f_data_char, NULL);
+		getcwd(cwd, 4096);
+		value = mk_str_lst(cwd);
+	}
+	else
+		return (ft_error("cd: fail"));
+	return (0);
+}
+
+int	builtin_cd(char **argv, t_lst *ev)
+{
+	char	*home_path;
+	t_lst	*value;
+
+	value = ft_hash_find(ev, "HOME");
+	home_path = get_env_value(value);
+	if (argv[1] == NULL && value == NULL)
+		return (ft_error("HOME not set"));
+	else if (argv[1] == NULL)
+		return (change_dir(home_path, ev));
+	else
+		return (change_dir(argv[1], ev));
+}
